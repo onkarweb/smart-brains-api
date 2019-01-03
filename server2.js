f8fb6af71eb053b43d828543c644816085cae67e
Onkar-Singh007 <35903798+Onkar-Singh007@users.noreply.github.com>
2019-01-03 12:48:00 -0500

    modifying database

--- server2.js
+++ server2.js
@@ -5,10 +5,8 @@ const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'root',
-    database : 'smartbrain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
